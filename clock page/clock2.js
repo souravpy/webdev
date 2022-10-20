891225b23561cb2550d6944866aa23a702fd94b6
Sourav Biswal <souravzsn@gmail.com>
2022-10-20 18:32:44 +0530

    increasd width of border in css, touched up JS

--- clock page/clock2.js
+++ clock page/clock2.js
@@ -9,11 +9,18 @@ function showTime() {
         let sec = time.getSeconds();
         am_pm = "AM";
         if (hour > 12) {
-                hour -= 12;
+                hour = 12;
                 am_pm = "PM";
         }
         if (hour == 0) {
                 hr = 12;
-                
+                am_pm = "AM";
         }
+hour = hour < 10 ? "0" + hour : hour;
+min = min < 10 ? "0" + min : min;
+sec = sec < 10 ? "0" + sec : sec;
+
+let currentTime = hour + ':' + min  + ":" + sec + am_pm
+document.getElementById("clock").innerHTML = currentTime;
 }
+showTime();
